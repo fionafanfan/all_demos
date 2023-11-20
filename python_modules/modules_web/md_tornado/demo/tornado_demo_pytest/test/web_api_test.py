f902ba8b923cb9a54ebfd543dc198db5183285ff
xianxiafan <fanxianxia@vv.cn>
2023-11-20 18:07:03 +0800

    fix: 添加metagpt和一些测试用例

--- python_modules/modules_web/md_tornado/demo/tornado_demo_pytest/test/web_api_test.py
+++ python_modules/modules_web/md_tornado/demo/tornado_demo_pytest/test/web_api_test.py
@@ -5,9 +5,10 @@
 # @File     : web_api_test.py
 # @Desc     :
 import requests
+import datetime
 
 
-def req_demo(testcase):
+def req_demo1(testcase):
     url = "http://127.0.0.1:8888/autotest/run"
     req_body = {"testcase": testcase}
 
@@ -18,11 +19,42 @@ def req_demo(testcase):
     return res
 
 
+def req_demo():
+    """
+[2023/11/16 18:05:21.869][INFO][auto_test] server_shape_indicator_check.py:1341 => begin---
+[2023/11/16 18:05:27.336][INFO][auto_test] server_shape_indicator_check.py:1344 => 姝ｅ湪璁＄畻: 00700.hk, min5
+    [2023/11/16 18:05:27.776][INFO][auto_test] server_shape_indicator_check.py:1347 => calculate ok
+[2023/11/16 18:05:27.778][INFO][auto_test] server_shape_indicator_check.py:1355 => choose
+[2023/11/16 18:05:27.779][INFO][auto_test] server_shape_indicator_check.py:1363 => end
+    :return:
+    """
+    # url = "http://127.0.0.1:8888/getShapeIndicator"
+    end_time = int(datetime.datetime.strptime("2023-11-17 2:00:00", "%Y-%m-%d %H:%M:%S").timestamp())
+    req_body = {"market": "us",
+                "kType": "min15",
+                "time": end_time,
+                "stockCode": "AAPL.us"
+                }
+    import time
+    s = time.time()
+
+    res = requests.post(url=url, json=req_body).json()
+    data = res.get("data", {})
+    print(type(data), data, time.time()-s)
+
+    return res
+
+
 if __name__ == '__main__':
-    # print(req_demo("hk_quotation_yidong_check").text)
-    # print(req_demo("hk_quotation_yidong_").text)
-    # print(req_demo("us_quotation_check").text)
-    print(req_demo("hk_shape").text)
-    # print(req_demo("us_shape").text)
+    print(req_demo())
+
+
+#
+# if __name__ == '__main__':
+#     # print(req_demo("hk_quotation_yidong_check").text)
+#     print(req_demo("hk_quotation_yidong_").text)
+#     # print(req_demo("us_quotation_check").text)
+#     # print(req_demo("hk_shape").text)
+#     # print(req_demo("us_shape").text)
 
 
