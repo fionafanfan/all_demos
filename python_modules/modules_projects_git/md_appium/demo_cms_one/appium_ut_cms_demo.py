b8a40e38f63997e70b15dfcf5dd3a9bc858dfa18
xianxiafan <fanxianxia@vv.cn>
2024-01-03 18:07:17 +0800

    fix: appium cms demo

--- python_modules/modules_projects_git/md_appium/demo_cms_one/appium_ut_cms_demo.py
+++ python_modules/modules_projects_git/md_appium/demo_cms_one/appium_ut_cms_demo.py
@@ -8,10 +8,25 @@
 """
 appium
 """
+import time
 import unittest
+from collections import namedtuple
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
+
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import (NoSuchElementException,
+                                        TimeoutException, UnexpectedAlertPresentException,
+                                        ElementNotInteractableException, ElementClickInterceptedException,
+                                        NoAlertPresentException, InvalidElementStateException,
+                                        StaleElementReferenceException, WebDriverException)
+
+from messages import LoginRequestMsg
+from messages import ServerEventIdType
 
 
 capabilities = {
@@ -41,6 +56,10 @@ capabilities = {
 
 
 appium_server_url = 'http://localhost:4723'
+
+
+class Index(object):
+    my = 'descriptionStartsWith("我的").index(4)'
 
 
 class Login(object):
@@ -94,16 +113,190 @@ class MyAppium(object):
     pass
 
 
+class _MyWebDriverWait(WebDriverWait):
+    """
+    继承WebDriverWait类，可以初始化后设置超时参数
+    """
+    @property
+    def timeout(self):
+        return self._timeout
+
+    @timeout.setter
+    def timeout(self, timeout):
+        self._timeout = timeout
+
+
+_IMPLICIT_WAIT_TIME = 10 # 全局隐式等待时间
+_EXPLICIT_WAIT_TIME = 3 # 全局显示等待时间
+
+# 元素定位信息
+ElementSelect = namedtuple('ElementSelector', [
+    # 名称、定位方法(By.XPATH、By.CSS_SELECTOR)、选择表达式、期望状态
+    # 期望状态: clickable、visible、located、text(内容不为空)
+    'name', 'method', 'selector', 'status'
+])
+
+
 class CmsBot(object):
     """
     招商国际app版本
     """
-    def login(self):
+    def __init__(self):
+        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+        self._wait: _MyWebDriverWait = None
+        self._wait = _MyWebDriverWait(self.driver, _EXPLICIT_WAIT_TIME, ignored_exceptions=(NoSuchElementException,
+                                                                                            KeyboardInterrupt))
+        self.login_parameter = LoginRequestMsg()
+
+    def is_element_clickable(self, method=By.CSS_SELECTOR, xpath_or_css_str='',
+                             wait_time=_EXPLICIT_WAIT_TIME):
         """
-        登录
+        界面元素在等待的时间内是否可点击
+        """
+        located = True
+        self.driver.implicitly_wait(wait_time)
+        self._wait.timeout = wait_time
+
+        try:
+            self._wait.until(EC.element_to_be_clickable((method, xpath_or_css_str)))
+        except TimeoutException:
+            located = False
+
+        self.driver.implicitly_wait(_IMPLICIT_WAIT_TIME)
+        self._wait.timeout = _EXPLICIT_WAIT_TIME
+
+        return located
+
+    def is_element_visible(self, method=By.CSS_SELECTOR, xpath_or_css_str='',
+                           wait_time=_EXPLICIT_WAIT_TIME):
+        """
+        界面元素在等待的时间内是否已经可见
+        """
+        located = True
+        self.driver.implicitly_wait(wait_time)
+        self._wait.timeout = wait_time
+
+        try:
+            self._wait.until(EC.visibility_of_element_located((method, xpath_or_css_str)))
+        except TimeoutException:
+            located = False
+
+        self.driver.implicitly_wait(_IMPLICIT_WAIT_TIME)
+        self._wait.timeout = _EXPLICIT_WAIT_TIME
+
+        return located
+
+    def is_element_located(self, method=By.CSS_SELECTOR, xpath_or_css_str='',
+                           wait_time=_EXPLICIT_WAIT_TIME):
+        """
+        界面元素在等待的时间内是否已经加载
+        """
+        located = True
+        self.driver.implicitly_wait(wait_time)
+        self._wait.timeout = wait_time
+
+        try:
+            self._wait.until(EC.presence_of_element_located((method, xpath_or_css_str)))
+        except TimeoutException:
+            located = False
+
+        self.driver.implicitly_wait(_IMPLICIT_WAIT_TIME)
+        self._wait.timeout = _EXPLICIT_WAIT_TIME
+
+        return located
+
+    def select_element(self, element_selects: [ElementSelect], wait_time=10):
+        """
+        选择元素
+        """
+        max_wait_time = time.time() + wait_time
+        name = ''
+        element_selects = [element_selects] if not isinstance(element_selects, list) else element_selects
+        while not name and time.time() < max_wait_time:
+            for element_select in element_selects:
+                if element_select.status == 'clickable':
+                    check_func = self.is_element_clickable
+                elif element_select.status == 'visible':
+                    check_func = self.is_element_visible
+                elif element_select.status == 'located':
+                    check_func = self.is_element_located
+                elif element_select.status == 'text':
+                    if not self.is_element_visible(element_select.method, element_select.selector, wait_time=0.5):
+                        continue
+                    check_func = self.driver.find_element
+                    if len(check_func(element_select.method, element_select.selector).text) > 0:
+                        name = element_select.name
+                        break
+                    continue
+                else:
+                    continue
+                if check_func(element_select.method, element_select.selector, wait_time=0.5):
+                    name = element_select.name
+                    break
+        return name
+
+    def on_login(self):
+        """
+        登录请求
+        :return:
+        """
+        if self.login_parameter.serverEventId == ServerEventIdType.StartConn:
+            self.login_start_conn()
+        elif self.login_parameter.serverEventId == ServerEventIdType.SendAcctPwd:
+            self.login_with_account()
+
+    def check_login_start(self):
+        """
+        判断启动连接成功还是失败
+        如果当前处于-我的 界面，则认为启动连接成功
+        :return:
+        """
+        my_el = self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, Index.my)
+
+        return True
+
+    def check_login_start_page(self):
+        """
+        确认是否已完成前置操作，处于可以进行交易登录条件
+        :return:
+        """
+        return True
+
+    def check_page_name(self):
+        """
+        判断页面状态
         :return:
         """
         pass
+
+    def login_start_conn(self):
+        """
+        启动连接:
+        13-启动连接成功: 返回启动连接成功信息，同时去检测当前是否已完成前置动作。
+        14-启动连接失败： 结束流程
+        :return:
+        """
+        is_start_success = self.check_login_start()
+        if is_start_success:
+            print("启动成功")
+            is_ready_login = self.check_login_start_page()
+            if is_ready_login:
+                print("前置操作已完成， 可以进行后续的账号密码登录动作")
+            else:
+                print("招商国际app未完成注册，请在手机端完成注册后，再次启动机器手连接")
+        else:
+            print("启动失败")
+
+    def login_with_account(self):
+        """
+        请求登录， 输入账号密码
+        :return:
+        """
+        page_name = self.check_page_name()
+        if page_name == 'login':
+            print("已登录")
+        elif page_name == 'logout':
+            pass
 
     def logout(self):
         """
@@ -116,14 +309,17 @@ class CmsBot(object):
         """确认当前是哪个页面"""
         pass
 
+    def clear(self):
+        if self.driver:
+            self.driver.quit()
+
 
 class TestAppium(unittest.TestCase):
     def setUp(self) -> None:
-        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+        self.bot = CmsBot()
 
     def tearDown(self) -> None:
-        if self.driver:
-            self.driver.quit()
+        self.bot.clear()
 
     # @unittest.skip('')
     # def test_find_battery(self):
@@ -149,22 +345,13 @@ class TestAppium(unittest.TestCase):
     #     capture_time_error_much_el = self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'descriptionContains("验证码错误次数过多")')
     #     print('capture_time_error_much_el>>', capture_time_error_much_el)
 
-    def test_find_goumaili(self):
-        el = self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'descriptionContains("最大购买力")')
-        print("buy money:", el)
-
+    @unittest.skip('')
     def test_input(self):
         pass
         # self.driver.find_element(AppiumBy.ID,'com.wetrade.financial:id/etUserName').send_keys('test')
 
-    # def test_login(self):
-    #     pass
-    #
-    # def test_logout(self):
-    #     pass
-    #
-    # def test_check_page(self):
-    #     pass
+    def test_login_start_conn(self):
+        self.bot.login_start_conn()
 
 
 if __name__ == '__main__':
